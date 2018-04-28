f041608de20727f4fbecfa3e4eb7f6c15ec5ac14
Dillon Christensen <j.dillon.christensen@gmail.com>
2018-04-28 12:40:07 -0500

    packaged into deployable server, still suffers from poor suffix matching though

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const http = require('http');
+const {parse} = require('url');
 const gather = require('./gather');
-gather(process.argv[2])
-    .then(data => console.log('DONE:', data))
-    .catch(err => console.error('FAIL:', err));
+
+http
+    .createServer((req, res) => {
+        const query = parse(req.url, true).query;
+        if (query && query.address) {
+            gather(process.argv[2])
+                .then(data => res.end(JSON.stringify(data)))
+                .catch(err => res.end('{}'));
+        }
+    })
+    .listen(process.env.PORT || 3000);
