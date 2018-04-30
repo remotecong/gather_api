82a4cb0d21fa95f8f2281651bf9c12f59f372f34
Dillon Christensen <dillon.christensen@kalibrate.com>
2018-04-30 11:24:17 -0500

    added https server instead

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
-const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const {parse} = require('url');
 const gather = require('./gather');
 
-http
-    .createServer((req, res) => {
-        res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
-        res.setHeader('Access-Control-Request-Method', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-        res.setHeader('Access-Control-Allow-Headers', '*');
+https
+    .createServer({
+            key: fs.readFileSync(process.env.SSL_KEY_PATH),
+            cert: fs.readFileSync(process.env.SSL_CERT_PATH)
+        },
+        (req, res) => {
+            res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+            res.setHeader('Access-Control-Request-Method', '*');
+            res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+            res.setHeader('Access-Control-Allow-Headers', '*');
 
-        if (req.method === 'OPTIONS') {
-            res.writeHead(200);
-            res.end();
-        } else {
-            const query = parse(req.url, true).query;
-            if (query && query.address) {
-                gather(query.address)
-                    .then(data => {
-                        res.writeHead(200);
-                        res.end(JSON.stringify(data))
-                    })
-                    .catch(err => {
-                        console.log(err);
-                        res.writeHead(500);
-                        res.end('{}');
-                    });
+            if (req.method === 'OPTIONS') {
+                res.writeHead(200);
+                res.end();
             } else {
-                res.writeHead(404);
-                res.end('hi');
+                const query = parse(req.url, true).query;
+                if (query && query.address) {
+                    gather(query.address)
+                        .then(data => {
+                            res.writeHead(200);
+                            res.end(JSON.stringify(data))
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.writeHead(500);
+                            res.end('{}');
+                        });
+                } else {
+                    res.writeHead(404);
+                    res.end('hi');
+                }
             }
-        }
-    })
+        })
     .listen(process.env.PORT || 3000);
