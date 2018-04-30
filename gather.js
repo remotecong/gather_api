7554c21baadce774d05712635973ab1246797556
Dillon Christensen <dillon.christensen@kalibrate.com>
2018-04-30 15:29:21 -0500

    optimized search, now using two pages to parallelize their loads ~4.5s for my address results (from 10 before DOMContentLoad change, and 6.5s with DOMContentLoadChange)

--- gather.js
+++ gather.js
@@ -1,49 +1,28 @@
 const puppeteer = require('puppeteer');
+const waitUntil = 'domcontentloaded';
+const ASSESSOR_URL = 'http://www.assessor.tulsacounty.org/assessor-property-search.php';
 const AGREE_BUTTON_SELECTOR = '[name="accepted"].positive';
-const STREET_TYPES = [{"options": ["avenue"], "value": "AV"}, {
-    "options": ["boulevard", "blvd"],
-    "value": "BV"
-}, {"options": ["circle"], "value": "CR"}, {"options": ["court", "ct"], "value": "CT"}, {
-    "options": ["drive"],
-    "value": "DR"
-}, {"options": ["expressway", "expwy"], "value": "EX"}, {
-    "options": ["highway", "hwy"],
-    "value": "HY"
-}, {"options": ["lane", "ln"], "value": "LN"}, {
-    "options": ["park", "pr", "pk"],
-    "value": "PK"
-}, {"options": ["place"], "value": "PL"}, {"options": ["road", "rd"], "value": "RD"}, {
-    "options": ["street"],
-    "value": "ST"
-}, {"options": ["terrace", "tr"], "value": "TE"}, {
-    "options": ["trail", "tl"],
-    "value": "TL"
-}, {"options": ["way", "wy"], "value": "WY"}];
+const STREET_TYPES = [
+    {options: ["street"], value: "ST"},
+    {options: ["avenue"], value: "AV"},
+    {options: ["place"], value: "PL"},
+    {options: ["road", "rd"], value: "RD"},
+    {options: ["court", "ct"], value: "CT"},
+    {options: ["circle"], value: "CR"},
+    {options: ["drive"], value: "DR"},
+    {options: ["lane", "ln"], value: "LN"},
+    {options: ["park", "pr", "pk"], value: "PK"},
+    {options: ["boulevard", "blvd"], value: "BV"},
+    {options: ["expressway", "expwy"], value: "EX"},
+    {options: ["highway", "hwy"], value: "HY"},
+    {options: ["terrace", "tr"], value: "TE"},
+    {options: ["trail", "tl"], value: "TL"},
+    {options: ["way", "wy"], value: "WY"}
+];
 
-
-module.exports = async address => {
-    if (!address) {
-        return {error: 'missing address'};
-    }
-
-    const assessorAddress = address.replace(/\./g, '').split(',').shift();
-    const assessorPieces = assessorAddress.match(/(\d+) ([NSEW]) ([^ ]+) (.*) ([A-Za-z]+)$/) || assessorAddress.match(/(\d+) ([NSEW]) ([^ ]+) ([^\s]+)$/);
-
-    if (!assessorPieces && assessorPieces.length < 6) {
-        return {error: 'bad address', input: address, decoded: assessorPieces};
-    }
-
-    const houseNumber = assessorPieces[1];
-    const direction = assessorPieces[2];
-    const streetName = assessorPieces[3];
-    const streetTypeValue = assessorPieces.pop().toLowerCase();
-    const streetType = (STREET_TYPES.find(t => t.options.includes(streetTypeValue) || t.options.some(o => o.includes(streetTypeValue))) || {value: 'ST'}).value;
-
-    console.log(houseNumber, direction, streetName, streetType);
-
-    const browser = await puppeteer.launch();
+const getOwnerData = async (browser, values) => {
     const page = await browser.newPage();
-    await page.goto('http://www.assessor.tulsacounty.org/assessor-property-search.php');
+    await page.goto(ASSESSOR_URL, {waitUntil});
 
     const shouldAccept = await page.$(AGREE_BUTTON_SELECTOR);
     if (shouldAccept) {
@@ -62,26 +41,45 @@ module.exports = async address => {
                 document.querySelector('#streetname').value = data.streetName;
                 document.querySelector('#streettype').value = data.streetType;
                 document.querySelector('#bttnaddr').click();
-            }, {houseNumber, streetName, streetType, direction})
+            }, values)
         ]);
     } catch (err) {
         console.log('Failed to do the assessor search', err);
         return {};
     }
 
-    const ownerData = await page.evaluate(houseNumber => {
+    return await page.evaluate(houseNumber => {
         const mailingAddress = Array.from(document.querySelectorAll('td')).find(cell => /Owner mailing address/i.test(cell.innerText)).nextElementSibling.innerHTML.replace(/<br>/g, ', ');
         const name = Array.from(document.querySelectorAll('td')).find(cell => /Owner name/i.test(cell.innerText)).nextElementSibling.textContent;
         const lastName = name.split(',').map(s => s.trim()).shift();
         const livesThere = mailingAddress.includes(houseNumber);
         return {mailingAddress, name, lastName, livesThere};
-    }, houseNumber);
+    }, values.houseNumber);
+};
 
+const getAssessorValues = address => {
+    const assessorAddress = address.replace(/\./g, '').split(',').shift();
+    const assessorPieces = assessorAddress.match(/(\d+) ([NSEW]) ([^ ]+) (.*) ([A-Za-z]+)$/) || assessorAddress.match(/(\d+) ([NSEW]) ([^ ]+) ([^\s]+)$/);
+
+    if (!assessorPieces || assessorPieces.length < 5) {
+        return {error: `Bad address\nGot ${address}\nDecoded as: ${JSON.stringify(assessorPieces)}`, input: address, decoded: assessorPieces};
+    }
+
+    const houseNumber = assessorPieces[1];
+    const direction = assessorPieces[2];
+    const streetName = assessorPieces[3];
+    const streetTypeValue = assessorPieces.pop().toLowerCase();
+    const streetType = (STREET_TYPES.find(t => t.options.includes(streetTypeValue) || t.options.some(o => o.includes(streetTypeValue))) || {value: 'ST'}).value;
+    return {houseNumber, streetName, streetType, direction};
+};
+
+const getThatsThemData = async (browser, address) => {
     const thatsThemURL = `https://thatsthem.com/address/${address.replace(/\./g, '').replace(/,? /g, '-')}`;
-    await page.goto(thatsThemURL, {timeout: 60000});
+    const page = await browser.newPage();
+    await page.goto(thatsThemURL, {timeout: 60000, waitUntil});
 
     try {
-        const phoneData = await page.evaluate(data => {
+        return await page.evaluate(() => {
             return Array.from(document.querySelectorAll('.ThatsThem-people-record.row'))
                 .map(result => {
                     const name = result.querySelector('h2').textContent.trim();
@@ -89,13 +87,31 @@ module.exports = async address => {
                     const isMobile = !!result.querySelector('[data-title="Mobile"] [itemprop="telephone"]');
                     return {name, number, isMobile};
                 })
-                .filter((p, i, a) => !!p.number && (p.name.toUpperCase().includes(data.lastName) || (!data.livesThere && !i)) && a.findIndex(ap => ap.number === p.number) === i);
-        }, ownerData);
-        await browser.close();
-        return {...ownerData, phones: phoneData};
+                .filter((p, i, a) => !!p.number && a.findIndex(ap => ap.number === p.number) === i);
+        });
     } catch (err) {
         console.log('thatsthem error:', err);
-        browser.close();
     }
-    return {};
+};
+
+module.exports = async address => {
+    if (!address) {
+        return {error: 'Missing address'};
+    }
+
+    const assessorValues = getAssessorValues(address);
+    if (!assessorValues || assessorValues.error) {
+        return assessorValues;
+    }
+
+    const browser = await puppeteer.launch();
+
+    const [ownerData, phoneData] = await Promise.all([
+        getOwnerData(browser, assessorValues),
+        getThatsThemData(browser, address)
+    ]);
+
+    await browser.close();
+
+    return {...ownerData, phones: phoneData.filter(p => (!ownerData.livesThere && !i) || p.name.toUpperCase().includes(ownerData.lastName))};
 };
