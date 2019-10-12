c4d381e4219ddcaf5f150283016553eec0c7c623
Dillon Christensen <j.dillon.christensen@gmail.com>
2019-10-11 23:14:00 -0500

    added some sentry tags

--- index.js
+++ index.js
@@ -6,33 +6,37 @@ Sentry.init({ dsn: process.env.SENTRY_URL});
 
 http
     .createServer((req, res) => {
-            res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
-            res.setHeader('Access-Control-Request-Method', '*');
-            res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-            res.setHeader('Access-Control-Allow-Headers', '*');
+        res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+        res.setHeader('Access-Control-Request-Method', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+        res.setHeader('Access-Control-Allow-Headers', '*');
+        Sentry.configureScope(scope => {
+            scope.setTag('ip', req.headers['x-forwarded-for'] || req.connection.remoteAddress);
+            scope.setTag('ua', req.headers['user-agent']);
+        });
 
-            if (req.method === 'OPTIONS') {
-                res.writeHead(200);
-                res.end();
+        if (req.method === 'OPTIONS') {
+            res.writeHead(200);
+            res.end();
+        } else {
+            const query = parse(req.url, true).query;
+            if (query && query.address) {
+                gather(query.address)
+                    .then(data => {
+                        res.writeHead(200);
+                        res.end(JSON.stringify(data))
+                    })
+                    .catch(err => {
+                        Sentry.captureException(err);
+                        console.log(err);
+                        res.writeHead(500);
+                        res.end('{}');
+                    });
             } else {
-                const query = parse(req.url, true).query;
-                if (query && query.address) {
-                    gather(query.address)
-                        .then(data => {
-                            res.writeHead(200);
-                            res.end(JSON.stringify(data))
-                        })
-                        .catch(err => {
-                            Sentry.captureException(err);
-                            console.log(err);
-                            res.writeHead(500);
-                            res.end('{}');
-                        });
-                } else {
-                    Sentry.captureException(new Error('no query submitted'));
-                    res.writeHead(404);
-                    res.end('hi');
-                }
+                Sentry.captureException(new Error('no query submitted'));
+                res.writeHead(404);
+                res.end('hi');
             }
-        })
+        }
+    })
     .listen(process.env.PORT || 3000);
