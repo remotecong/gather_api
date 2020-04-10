a81c4d02a4db09eb0558e198aa132c5eb745d8aa
Dillon Christensen <j.dillon.christensen@gmail.com>
2020-04-09 20:46:29 -0500

    cleaned up server code

--- src/index.js
+++ src/index.js
@@ -4,43 +4,59 @@ const gather = require("./gather");
 const Sentry = require("@sentry/node");
 Sentry.init({ dsn: process.env.SENTRY_URL });
 
-http
-  .createServer((req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
-    res.setHeader("Access-Control-Request-Method", "*");
-    res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    Sentry.configureScope(scope => {
-      scope.clear();
-      scope.setTag("ip", req.connection.remoteAddress);
-      scope.setTag("ua", req.headers["user-agent"]);
-    });
+function getIp(req) {
+  try {
 
-    if (req.method === "OPTIONS") {
-      res.writeHead(200);
-      res.end();
-    } else {
+    return (req.headers['x-forwarded-for'] || '').split(',').pop() ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress;
+  } catch (ignore) {
+    return null;
+  }
+}
+
+http
+  .createServer(async (req, res) => {
+    try {
+      res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+      res.setHeader("Access-Control-Request-Method", "*");
+      res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+      res.setHeader("Access-Control-Allow-Headers", "*");
       const query = parse(req.url, true).query;
-      if (query && query.address) {
-        gather(query.address)
-          .then(data => {
-            res.writeHead(200);
-            res.end(JSON.stringify(data));
-          })
-          .catch(err => {
-            Sentry.withScope(scope => {
-              scope.setTag("query", query.address);
-              Sentry.captureException(err);
-            });
-            console.log(err);
-            res.writeHead(500);
-            res.end("{}");
-          });
-      } else {
+
+      Sentry.configureScope(scope => {
+        scope.clear();
+        scope.setTag("ip", getIp(req));
+        scope.setTag("ua", req.headers["user-agent"]);
+        scope.setTag("query", query.address || 'NO ADDRESS');
+      });
+
+      if (req.method === "OPTIONS") {
+        res.writeHead(200);
+        res.end();
+        return;
+      }
+
+
+      if (!query || !query.address) {
         Sentry.captureException(new Error("no query submitted"));
         res.writeHead(404);
         res.end("hi");
+        return;
       }
+
+      const data = await gather(query.address);
+
+      res.writeHead(200, {
+        'Content-Type': 'application/json',
+      });
+      res.end(JSON.stringify(data));
+    } catch(err) {
+      console.log(err);
+      res.writeHead(500);
+      Sentry.captureException(err);
+      res.end("{}");
     }
   })
   .listen(process.env.PORT || 3000);
